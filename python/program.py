5d15062b5e3b5c52c5c944ac376b25bf609e01ec
BuildTools <unconfigured@null.spigotmc.org>
2022-05-30 23:15:46 +0200

    Python code update

--- python/program.py
+++ python/program.py
@@ -1,5 +1,20 @@
- >>> import serial
->>> ser = serial.Serial('/dev/ttyACM1')  # open serial port
->>> print(ser.name)         # check which port was really used
->>> ser.write(b'5T\n')     # write a string
->>> ser.close()
+import serial
+import time as t
+
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.setPort('/dev/ttyACM1')
+
+ser.open()
+print(ser.name)
+ser.write(b"5T\n")
+t.sleep(3)
+print("START")
+ser.write(b'5P0000 0000\n')
+t.sleep(0.03)
+ser.write(b'5P0000 2000\n')
+t.sleep(3)
+ser.write(b'5P0000 0000\n')
+
+ser.close()
+print("END")
